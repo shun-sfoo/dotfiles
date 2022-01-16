86a510b0d1bd0874f0dadcf5c4d9f28cc4330afc
shun-sfoo <ganymede0915@gmail.com>
2022-01-16 15:32:56 +0800

    update

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,16 +1,13 @@
 use std::{fs, process::Command};
 
 const ENV_PATH: &str = "/Sources/dotfiles/env";
-const _CABAL_DIR: &str = "/.cabal";
-const _STACK_DIR: &str = "/.stack";
 
 fn main() {
     link();
-    haskell();
 }
 
 fn link() {
-    println!("READY TO LINK");
+    println!("\x1b[34mREADY TO LINK\x1b[0m");
     let home_dir = std::env::var("HOME").expect("MUST RUN IN USER");
     let dotfile_dir = home_dir.clone() + ENV_PATH;
     let path = fs::read_dir(dotfile_dir).expect("DOTFILES PATH IS ERROR");
@@ -19,7 +16,7 @@ fn link() {
         let dir = entry.expect("CHILD FILE IS ERROR");
         let original_name = dir.file_name();
         let link = home_dir.clone() + "/." + &original_name.clone().into_string().expect("Error");
-        println!("ln -s {:?} {:?}", dir.path(), link);
+        println!("\x1b[32mln -s {:?} {:?}\x1b[0m", dir.path(), link);
         let _task = Command::new("ln")
             .arg("-s")
             .arg(dir.path())
@@ -27,11 +24,5 @@ fn link() {
             .output()
             .expect("failed");
     }
-    println!("link DOWN");
-}
-
-fn haskell() {
-    println!("CONFIGURATION HASKELL ENVIROMENT");
-
-    println!("HASKELL ENVIROMENT DONE");
+    println!("\x1b[34mLINK DOWN\x1b[0m");
 }
